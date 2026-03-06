aa79c8018316b8d3c8eae3a37460ce0ecc991842
Hannahimtiaz <hannahkhan680@gmail.com>
2026-03-06 15:25:10 +0530

    UC12:Hardcoded palindrome

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,31 +1,35 @@
 
 
-    public class palindromecheckerapp {
+import java.util.Stack;
+public class palindromecheckerapp {
         public static void main(String[] args){
 
-            String input = "madam";
-
-            PalindromeService service = new PalindromeService();
-            boolean result = service.checkPalindrome(input);
+            String input = "level";
+            PalindromeStrategy strategy = new StackStrategy();
+            boolean result = strategy.check(input);
 
             System.out.println("Input : " + input);
             System.out.println("Is Palindrome? : " + result);
         }
     }
 
-    class PalindromeService {
+    interface PalindromeStrategy {
+        boolean check(String input);
+    }
 
-        public boolean checkPalindrome(String input) {
+    class StackStrategy implements PalindromeStrategy {
 
-            int start = 0;
-            int end = input.length() - 1;
+        public boolean check(String input) {
+            Stack<Character> stack = new Stack<>();
 
-            while (start < end) {
-                if (input.charAt(start) != input.charAt(end)) {
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+            }
+
+            for (char c : input.toCharArray()) {
+                if (c != stack.pop()) {
                     return false;
                 }
-                start++;
-                end--;
             }
 
             return true;
